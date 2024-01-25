2065137713ecd12696d4ef42974b485424dff173
JulianLargo72 <julianbetancur104@gmail.com>
2024-01-25 10:50:15 -0500

    crea el qr

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pyqrcode
+import os
+
+app = Flask(__name__)
+
+def generar_qr(identificacion, nombre):
+    id_completo = '65' + str(identificacion)
+    info_completa = f"{id_completo} - {nombre}"
+    qr = pyqrcode.create(info_completa, error='L')
+    qr_path = f'static/qr_images/A{identificacion}.png'
+    qr.png(qr_path, scale=6)
+    return qr_path
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            identificacion = int(request.form['identificacion'])
+            nombre = request.form['nombre']
+            qr_path = generar_qr(identificacion, nombre)
+            mensaje = f"QR generado para la identificación {identificacion} y el nombre {nombre}"
+            return render_template('index.html', mensaje=mensaje, qr_path=qr_path)
+        except ValueError:
+            mensaje = "Por favor, ingrese un número entero válido."
+            return render_template('index.html', mensaje=mensaje)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
